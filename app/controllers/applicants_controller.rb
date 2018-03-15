c304d88142e71f72a4f167102dacead8de5d5978
Matt L <matt@Matts-MacBook-Pro-2.local>
2018-03-14 19:10:20 -0700

    work out session logic

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -22,29 +22,39 @@ class ApplicantsController < ApplicationController
   def edit
   end
 
-  # POST /applicants
-  # POST /applicants.json
   def create
     @applicant = Applicant.new(applicant_params)
+    # instead of saving it lets imagine that I sent it to some API, and if i get a 200 back i will redirect over
     if @applicant.save
-      @applicant.status = 0
-      @applicant.save
-      redirect_to applicant_path
+      redirect_to thank_you_applicants_path
     else
-      flash[:error] = @applicant.full_messages.join(',')
+      
       redirect_to :back
     end
   end
-end
 
-
-private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_applicant
-      @applicant = Applicant.find(params[:id])
+  # POST /applicants
+  # POST /applicants.json
+  def create_conf
+    session[:email] = applicant_params[:email]
+    if session[applicant_params[:email]].nil?
+      session[applicant_params[:email]] = {}
     end
-
-    def applicant_params
-      params.require(:applicant).permit(:id, :first_name, :last_name, :email, :phone, :zip_code)
-    end
+    session[applicant_params[:email]][:first_name] = applicant_params[:first_name]
+    session[applicant_params[:email]][:last_name] = applicant_params[:last_name]
+    session[applicant_params[:email]][:email] = applicant_params[:email]
+    session[applicant_params[:email]][:phone] = applicant_params[:phone]
+    session[applicant_params[:email]][:zip_code] = applicant_params[:zip_code]
   end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_applicant
+    @applicant = Applicant.find(params[:id])
+  end
+
+  def applicant_params
+    params.require(:applicant).permit(:id, :first_name, :last_name, :email, :phone, :zip_code, :referrer)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :applicants
+  resources :applicants do 
+  	collection do 
+	  	get 'create_conf'
+	  	get 'thank_you'
+		end
+  end
   root 'applicants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
